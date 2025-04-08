9fb6fd6308a831a408db4e2abc1d0251a19a109b
Krystian <226311@student.uek.krakow.pl>
2025-04-09 00:45:03 +0200

    feat: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import DashboardLayout from "./components/shared/navbar/DashboardLayout";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import LoginForm from "@components/auth/LoginForm";
+import RegisterForm from "@components/auth/RegisterForm";
+import Toast from "@components/shared/Toast";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Routes>
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+      </Routes>
 
-export default App
+      <Toast />
+    </>
+  );
+};
+
+export default App;
