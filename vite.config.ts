f5786da6044b5b91d3fc735ab86d654735839378
Krystian <226311@student.uek.krakow.pl>
2025-04-09 17:26:42 +0200

    chore: app dependency Redux Persist

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       "@components": new URL("./src/components", import.meta.url).pathname,
       "@pages": new URL("./src/pages", import.meta.url).pathname,
       "@fbconfig": new URL("./src/firebase", import.meta.url).pathname,
+      "@features": new URL("./src/features", import.meta.url).pathname,
     },
   },
 });
