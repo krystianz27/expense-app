642be6b32b24fd156ecbf2a72d7e2a5a77f0a8b1
Krystian <226311@student.uek.krakow.pl>
2025-04-10 21:39:34 +0200

    chore: remove mui/toolpad dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-import { URL } from "url";
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      "@src": new URL("./src", import.meta.url).pathname,
       "@components": new URL("./src/components", import.meta.url).pathname,
       "@pages": new URL("./src/pages", import.meta.url).pathname,
       "@fbconfig": new URL("./src/firebase", import.meta.url).pathname,
       "@features": new URL("./src/features", import.meta.url).pathname,
+      "@lib": new URL("./src/lib", import.meta.url).pathname,
     },
   },
 });
