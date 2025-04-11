705fcaff60a5f5803149420fdfb23f8911af7407
Krystian <226311@student.uek.krakow.pl>
2025-04-12 01:22:16 +0200

    feat: add Spinner

---
+++ src/components/shared/Spinner copy.tsx
@@ -0,0 +1,9 @@
+import { BounceLoader } from "react-spinners";
+
+const Spinner = () => (
+  <div className="flex justify-center items-center">
+    <BounceLoader size={50} color="#36d7b7" />
+  </div>
+);
+
+export default Spinner;
---
+++ src/components/shared/Spinner.tsx
@@ -0,0 +1,9 @@
+import { MoonLoader } from "react-spinners";
+
+const Spinner = () => (
+  <div className="mt-0 flex justify-center items-center min-h-screen">
+    <MoonLoader size={300} color="#36d7b7" />
+  </div>
+);
+
+export default Spinner;
