c27952013fdb365163b2128eac7741a14c9223f1
Krystian <226311@student.uek.krakow.pl>
2025-04-12 18:31:44 +0200

    feat: add PWA functionality

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,51 @@ import { defineConfig } from "vite";
 import { visualizer } from "rollup-plugin-visualizer";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer({ open: true })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({ open: true }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Expense Tracker",
+        short_name: "ExpTrack",
+        description:
+          "Expense Tracker is a simple and easy to use expense tracking app.",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "android-launchericon-192-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-launchericon-512-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@src": new URL("./src", import.meta.url).pathname,
